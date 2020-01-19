eea1d7f8500e20fcd68facf20c9eb6b4a61886bf
Việt Anh <nguyenvietanh.ad@gmail.com>
2020-01-20 02:47:46 +0700

    Update submit.php

--- submit.php
+++ submit.php
@@ -28,7 +28,7 @@ if(isset($_POST['data'])) {
       
       if(isset($data['token']) && $data['token'] != '') {
             $token = $data['token'];
-            $sql_select = "SELECT id FROM survey where token=`".$token.'`';
+            $sql_select = "SELECT id FROM survey where token=".$token;
             $result = $conn->query($sql_select);
             if ($result->num_rows > 0) {
                   return 3;
