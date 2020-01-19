cab2404e13561ed10b6d4d80300c6b376408ea51
Việt Anh <nguyenvietanh.ad@gmail.com>
2020-01-20 01:45:25 +0700

    Update submit.php

--- submit.php
+++ submit.php
@@ -47,7 +47,7 @@ if(isset($_POST['data'])) {
             die;
       }
       
-      $sql = "INSERT INTO survey (token, quality-store, quality-stage, space-stage, duration-program, quality-lunching, like_thing, channel-resource, channel-resource-other, improve_thing, created_at) 
+      $sql = "INSERT INTO survey (token, qualityStore, qualityStage, spaceStage, durationProgram, qualityLunching, likeThing, channelResource, channelResourceOther, improveThing, created_at) 
                         VALUES ($token, $quality_store, $quality_stage,  $space_stage,  $duration_program,  $quality_lunching,  $like_thing,  $channel_resource,  $channel_resource_other, $improve_thing, $time)";
       if (mysqli_query($conn, $sql)) {
             echo "New record created successfully";
